8cb6cacd1b785e46763532cb1b47e2ee6fc8fe01
stock-nicolas <1stockingernicolas@gmail.com>
2024-02-16 09:36:30 +0100

    Bouttons d'activités

--- app/src/main/java/com/example/tp_leboncoin/MainActivity.java
+++ app/src/main/java/com/example/tp_leboncoin/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.tp_leboncoin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
 
         Button button1 = findViewById(R.id.button1);
         Button button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,AdListViewActivity.class));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,AdAddActivity.class));
+            }
+        });
     }
+
+
 }
