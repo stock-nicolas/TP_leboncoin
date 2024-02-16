09fbd97c2957f32cd9494f7bd558023c33e48672
stock-nicolas <1stockingernicolas@gmail.com>
2024-02-16 09:46:27 +0100

    Bouttons d'activités

---
+++ app/src/main/java/com/example/tp_leboncoin/AdAddActivity.java
@@ -0,0 +1,13 @@
+package com.example.tp_leboncoin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdAddActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
