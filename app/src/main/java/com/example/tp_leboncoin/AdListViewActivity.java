01de6ae214851af26a9efd5758c04caf708a096d
stock-nicolas <1stockingernicolas@gmail.com>
2024-02-16 09:08:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tp_leboncoin/AdListViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.tp_leboncoin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdListViewActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/tp_leboncoin/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.tp_leboncoin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+    }
+}
