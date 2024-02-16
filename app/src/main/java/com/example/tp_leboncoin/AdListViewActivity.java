5be25d6b738f88bb863ba47be8614e0c62c712ef
Hugo <hugojoly2803@gmail.com>
2024-02-16 11:33:20 +0100

    list admodel

--- app/src/main/java/com/example/tp_leboncoin/AdListViewActivity.java
+++ app/src/main/java/com/example/tp_leboncoin/AdListViewActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AdListViewActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdModel Annonce1 = new AdModel("caillou", "Lahure", 1);
+        AdModel Annonce2 =  new AdModel("béton", "Lahure", 2);
+        AdModel Annonce3 = new AdModel("métaux", "Lahure", 3);
+
+        ArrayList<AdModel> liste_annonce = new ArrayList<>() ;
+
+        liste_annonce.add(Annonce1);
+        liste_annonce.add(Annonce2);
+        liste_annonce.add(Annonce3);
     }
 }
+
+
