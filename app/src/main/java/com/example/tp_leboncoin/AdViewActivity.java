1902f9f68b7cf7b9e57cd95bef86228ac65f10c4
Baptiste Plagne <97460466+BaptistePLAGNE@users.noreply.github.com>
2024-02-16 12:43:04 +0100

    View Activity

--- app/src/main/java/com/example/tp_leboncoin/AdViewActivity.java
+++ app/src/main/java/com/example/tp_leboncoin/AdViewActivity.java
@@ -1,6 +1,8 @@
 package com.example.tp_leboncoin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +10,16 @@ public class AdViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listview_ad);
+        setContentView(R.layout.activity_view_ad);
+        Intent i = getIntent();
+        String TITLE = i.getStringExtra ("title");
+        String ADRESSE = i.getStringExtra ("adresse");
+
+        TextView Title = findViewById(R.id.textView5);
+        TextView Adresse = findViewById(R.id.textView6);
+
+        Title.setText(TITLE);
+        Adresse.setText(ADRESSE);
+
     }
 }
