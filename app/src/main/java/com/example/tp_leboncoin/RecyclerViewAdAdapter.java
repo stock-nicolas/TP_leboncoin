e81c21853ba741ee2d8a27a8dd8582dc85ba9afc
Baptiste Plagne <97460466+BaptistePLAGNE@users.noreply.github.com>
2024-02-21 10:20:19 +0100

    Card View

--- app/src/main/java/com/example/tp_leboncoin/RecyclerViewAdAdapter.java
+++ app/src/main/java/com/example/tp_leboncoin/RecyclerViewAdAdapter.java
@@ -1,6 +1,7 @@
 package com.example.tp_leboncoin;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class RecyclerViewAdAdapter extends RecyclerView.Adapter<RecyclerViewAdAdapter.RecyclerViewHolder>{
-    private List<AdModel> data;
-    public RecyclerViewAdAdapter(List<AdModel> data) {this.data = data;}
+
+    public interface OnItemClickListener {
+        void onItemClick(AdModel item);
+    }
+    private final List<AdModel> data;
+
+    private final OnItemClickListener listener;
+    public RecyclerViewAdAdapter(List<AdModel> data,OnItemClickListener listener) {this.data = data;this.listener = listener;}
     @NonNull
     @Override
     public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -26,15 +33,12 @@ public class RecyclerViewAdAdapter extends RecyclerView.Adapter<RecyclerViewAdAd
 
     public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
 // This method is called for each of the visible rows displayed in our RecyclerView. It is usually here that we update their appearance.
-                AdModel ad = data.get(position);
-        holder.titleTextView.setText(ad.getTitle());
-        holder.addressTextView.setText(ad.getAddress());
-        holder.imageView.setImageResource(ad.getImage());
-        System.out.println(position);
+        AdModel ad = data.get(position);
+        holder.bind(ad,listener);
     }
     @Override
     public int getItemCount() {return data.size();}
-    public static class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder {
         public final ImageView imageView;
         public final TextView titleTextView;
         public final TextView addressTextView;
@@ -45,9 +49,15 @@ public class RecyclerViewAdAdapter extends RecyclerView.Adapter<RecyclerViewAdAd
             addressTextView = itemView.findViewById(R.id.textView2);
         }
 
-
-        @Override
-        public void onClick(View v) {
+        public void bind(final AdModel item, final OnItemClickListener listener) {
+            titleTextView.setText(item.getTitle());
+            addressTextView.setText(item.getAddress());
+            imageView.setImageResource(item.getImage());
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    listener.onItemClick(item);
+                }
+            });
         }
     }
 }
