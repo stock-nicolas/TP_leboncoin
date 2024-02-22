4649aad740992ccc30c0288753e0734941359734
Hugo <hugojoly2803@gmail.com>
2024-02-22 22:05:02 +0100

    main initdb

--- app/src/main/java/com/example/tp_leboncoin/MainActivity.java
+++ app/src/main/java/com/example/tp_leboncoin/MainActivity.java
@@ -21,35 +21,28 @@ public class MainActivity extends AppCompatActivity {
         Button button2 = findViewById(R.id.button2);
         Button button3 = findViewById(R.id.Boutton_Init_DB);
 
+        Context cont = this;
+        DBManager dbManager = DBManager.getDBManager(cont);
+        dbManager.open();
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,AdListViewActivity.class));
+                startActivity(new Intent(MainActivity.this, AdListViewActivity.class));
+                Cursor cursor = dbManager.fetch();
+                if (cursor.getCount() == 0) {
+                    dbManager.init();
+                }
             }
         });
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,AdAddActivity.class));
+                startActivity(new Intent(MainActivity.this, AdAddActivity.class));
             }
         });
 
-        Context cont = this;
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DBManager dbManager = DBManager.getDBManager(cont);
-                dbManager.open();
-                Cursor cursor = dbManager.fetch();
-                if(cursor.getCount() == 0){
-                    dbManager.init();
-                }
-            }
-        });
+
     }
-
-
-
 
 }
